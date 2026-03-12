f786dc6255a2690067a4a293e7ad1db7e94cc6bc
Quinten <110990931+Quinten0320@users.noreply.github.com>
2026-03-12 15:57:42 +0100

    Basic map

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,28 +6,30 @@ namespace Project;
 
 public class Game1 : Game
 {
-    private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private DungeonMap _dungeonMap;
 
     public Game1()
     {
-        _graphics = new GraphicsDeviceManager(this);
+        _ = new GraphicsDeviceManager(this)
+        {
+            PreferredBackBufferWidth = DungeonMap.Cols * DungeonMap.TileSize,   // 800
+            PreferredBackBufferHeight = DungeonMap.Rows * DungeonMap.TileSize,  // 480
+        };
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
     }
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
+        _dungeonMap = new DungeonMap();
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
+        _dungeonMap.LoadContent(GraphicsDevice);
     }
 
     protected override void Update(GameTime gameTime)
@@ -35,16 +37,16 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
-
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.Black);
 
-        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _dungeonMap.Draw(_spriteBatch);
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
