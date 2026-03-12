d6aae2fad4e953292e78593390b419976fcaeb94
Jeroenn89 <s1174675@student.windesheim.nl>
2026-03-12 14:56:27 +0100

    BaseGameEntity

---
+++ World/GameWorld.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.Xna.Framework;
+
+namespace AAI_MonogameAssignment
+{
+    public class GameWorld
+    {
+        public GameWorld()
+        {
+
+        }
+    }
+}
